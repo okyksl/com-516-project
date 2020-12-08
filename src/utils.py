4295d06cbc4ede42063cb12a72aff1d79a6511db
Oğuz Kaan Yüksel <okyksl@gmail.com>
2020-12-09 01:05:50 +0300

    add random generator utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,19 @@
 import logging
+from typing import List
+
+import numpy as np
 from omegaconf import DictConfig, OmegaConf
+
+def rand() -> float:
+    return np.random.rand()
+
+def rand_bool() -> bool:
+    if rand() < 0.5:
+        return True
+    return False
+
+def rand_bool_n(n: int) -> List[bool]:
+    return [ rand_bool() for i in range(n) ]
 
 def display_config(cfg: DictConfig) -> None:
     """Displays the configuration"""
